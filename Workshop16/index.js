ff6b82fc2acfa9f5fac894ae45a6efaca102a52f
iamdaehyun <126280104+iamdaehyun@users.noreply.github.com>
2023-05-01 15:30:19 -0700

    workshop16  final

--- Workshop16/index.js
+++ Workshop16/index.js
@@ -1,4 +1,4 @@
-//customer objects
+//copy and paste customer objects
 
 const timmy = {
   prescription: "acetaminophen",
@@ -24,14 +24,20 @@ const rocky = {
   coupon: true,
 };
 
+//function calculate total
 function calculateTotal(customer) {
+  //refill total is pricePerRefill x refills
   let refillTotal = customer.pricePerRefill * customer.refills;
+  //if subscription true, then will get 25% off.
   let subscriptionDiscount = customer.subscription ? refillTotal * 0.25 : 0;
+  //if coupon is true, then $10 discount.
   let couponDiscount = customer.coupon ? 10 : 0;
+  //the final amount will be refill total - subscription - coupon
   let finalAmount = refillTotal - subscriptionDiscount - couponDiscount;
   return `Your grand total is $${finalAmount}.`;
 }
 
+//total calculation of timmy, sarah, rocky
 console.log(calculateTotal(timmy));
 console.log(calculateTotal(sarah));
 console.log(calculateTotal(rocky));
