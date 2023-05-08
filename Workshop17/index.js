ea252753453ec6a472b5be1afff139dc268672c2
iamdaehyun <126280104+iamdaehyun@users.noreply.github.com>
2023-05-08 15:40:12 -0700

    completed

--- Workshop17/index.js
+++ Workshop17/index.js
@@ -2,36 +2,30 @@
 const coffeeMenu = require("./coffee_data");
 
 //Print an array of all the drinks on the menu.
-const printName = (drinks) => {
-  return drinks.name;
-};
-console.log(coffeeMenu.map(printName));
+const drinkNames = coffeeMenu.map((drink) => drink.name);
+console.log(drinkNames);
 
 //Print an array of drinks that cost 5 and under.
-const underFive = (drinks) => {
-  return drinks.price <= 5;
-};
-const drinksUnderFive = coffeeMenu.filter(underFive);
-console.log(drinksUnderFive);
+const underFive = coffeeMenu.filter((drink) => drink.price <= 5);
+console.log(underFive);
 
 //Print an array of drinks that are priced at an even number.
-const evenNumber = (drinks) => {
-  return drinks.price % 2;
-};
-const drinksEvenNumber = coffeeMenu.filter(evenNumber);
-console.log(drinksEvenNumber);
+const evenPrice = coffeeMenu.filter((drink) => drink.price % 2 === 0);
+console.log(evenPrice);
 
 //Print the total if you were to order one of every drink.
-//const sumTotalPrice = coffeeMenu.price.reduce
-//    (accumulator, currentValue) => accumulator + currentValue,
-//console.log(sumTotalPrice);
+const totalPrice = coffeeMenu.reduce((total, drink) => total + drink.price, 0);
+console.log("Total price if you were to order one of every drink:", totalPrice);
 
 //Print an array with all the drinks that are seasonal.
-const seasonalMenu = (drinks) => {
-  if ((drinks.seasonal = true));
-};
-const seasonalMenuArray = coffeeMenu.filter(seasonalMenu);
-console.log(seasonalMenuArray);
+const seasonalDrinks = coffeeMenu.filter((drink) => drink.seasonal);
+console.log(seasonalDrinks);
 
 //Print all the seasonal drinks with the words "with imported beans"
 //after the item name.
+
+const seasonalDrinksWithImportedBeans = coffeeMenu
+  .filter((drink) => drink.seasonal)
+  .map((drink) => drink.name + " with imported beans");
+
+console.log(seasonalDrinksWithImportedBeans);
